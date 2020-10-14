536224aba6408cd1439c5343bd74d90caeb5e65d
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2020-10-13 22:13:05 -0300

    inicio do backend

--- aulas/backend/src/server.ts
+++ aulas/backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/orphanage'
 
 import './database/connection'
+
+
 
 const app = express()
 app.use(express.json())
@@ -21,11 +25,37 @@ app.use(express.json())
 //Route Parms: http://localhost:3333/users/1 (Identificam um recurso)
 //Body: http://localhost:3333/users/1 (Identificam um recurso)
 
-app.get('/users', (request, response)=>{
-    return response.json({message: 'Hello World'})
+app.post('/orphanages', async (request, response) => {
+
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+
+    await orphanagesRepository.save(orphanage)
+
+    return response.status(201).json(orphanage)
 })
 
 app.listen(3333);
 
 
-//35:16
+//58:52
+//https://nextlevelweek.com/episodios/omnistack/2/edicao/3?utm_source=convertkit&utm_medium=email&utm_campaign=NLW3+Workshops&utm_term=Leads+OmniStack&utm_content=Workshop+2
