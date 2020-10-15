9ddce3d532ce072522bd35cbdcd0eaab057b8ab8
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2020-10-14 22:46:32 -0300

    backend finalizado

--- aulas/backend/src/controllers/OrphanagesController.ts
+++ aulas/backend/src/controllers/OrphanagesController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
-import Orphanage from '../models/orphanage'
+import * as Yup from 'yup'
 
+import OrphanageView from '../views/orphanages_view'
+import orphanages_view from '../views/orphanages_view'
+import Orphanage from '../models/orphanage'
 
 export default {
     async create(request: Request, response: Response) {
@@ -17,11 +20,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage)
         const requestImages = request.files as Express.Multer.File[]
-        const images = requestImages.map(image=> {
-            return {path: image.filename}
+        const images = requestImages.map(image => {
+            return { path: image.filename }
         })
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -30,7 +33,26 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),//name: Yup.string().required('Nome obrigatório'),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         })
+
+        await schema.validate(data, {
+            abortEarly: false
+        })
+
+        const orphanage = orphanagesRepository.create(data)
 
         await orphanagesRepository.save(orphanage)
 
@@ -40,27 +62,31 @@ export default {
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage)
 
-        const orphanages = await orphanagesRepository.find()
-        return response.json(orphanages)
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        })
+        return response.json(orphanages_view.renderMany(orphanages))
     },
 
     async show(request: Request, response: Response) {
         const { id } = request.params
         const orphanagesRepository = getRepository(Orphanage)
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id)
-        return response.json(orphanage)
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        })
+        return response.json(OrphanageView.render(orphanage))
     }
 }
 
 /*
 {
-	"name": "Lar das meninas",
-	"latitude": -27.2104339,
-	"longitude": -49.629111,
-	"about": "Sobre o orfanato",
-	"instructions": "Venha visitar",
-	"opening_hours": "Das 8h até 18h",
-	"open_on_weekends": true
+    "name": "Lar das meninas",
+    "latitude": -27.2104339,
+    "longitude": -49.629111,
+    "about": "Sobre o orfanato",
+    "instructions": "Venha visitar",
+    "opening_hours": "Das 8h até 18h",
+    "open_on_weekends": true
 }
 */
