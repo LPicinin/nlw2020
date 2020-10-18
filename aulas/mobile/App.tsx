224f198339bf38927a4e8f59ac7dc331a61ebe59
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2020-10-18 19:59:07 -0300

    sincro

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello NLW!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -22.1483974,
+          longitude: -51.1771001,
+          latitudeDelta: 0.08,
+          longitudeDelta: 0.08
+        }}
+      />
     </View>
   );
 }
@@ -14,10 +23,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 });
 
+//39:09
 //https://nextlevelweek.com/episodios/omnistack/5/edicao/3?utm_source=convertkit&utm_medium=email&utm_campaign=NLW3+Workshops&utm_term=Leads+OmniStack&utm_content=Workshop+5
