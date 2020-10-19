fa6a6a81079e841a1e2e019307c256764383d391
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2020-10-18 22:01:54 -0300

    4ª aula da nlw concluida, falta só mais uma aula wow
    react é zika d+

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,34 +1,28 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
+
+import Routes from './src/routes'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -22.1483974,
-          longitude: -51.1771001,
-          latitudeDelta: 0.08,
-          longitudeDelta: 0.08
-        }}
-      />
-    </View>
-  );
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if(!fontsLoaded){
+    return null
+  }
+
+  return ( 
+    <Routes/>
+   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  }
-});
 
 //39:09
 //https://nextlevelweek.com/episodios/omnistack/5/edicao/3?utm_source=convertkit&utm_medium=email&utm_campaign=NLW3+Workshops&utm_term=Leads+OmniStack&utm_content=Workshop+5
